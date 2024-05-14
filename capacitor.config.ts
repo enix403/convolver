4a582cc838cef1eb6deb69fb5e39208ae178a4e9
enix403 <fog.code000@gmail.com>
2024-05-14 18:27:57 +0500

    Add capacitor to the project

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.radium.convolveapp',
+  appName: 'Convolve',
+  webDir: 'dist'
+};
+
+export default config;
